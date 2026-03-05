1a68d62b8aa68bf621034fef7a913f9e3f5527ac
Reda Morsli <47378462+RedaMorsli@users.noreply.github.com>
2026-03-05 12:02:21 -0500

    Minor fixes

--- frontend/pages/project_detail/infra/empty_infra_menu.gd
+++ frontend/pages/project_detail/infra/empty_infra_menu.gd
@@ -8,10 +8,12 @@ signal infra_created()
 @onready var buttons: HBoxContainer = %Buttons
 @onready var loading_spinner: TextureRect = %LoadingSpinner
 @onready var infra_config_form: InfraConfigForm = %InfraConfigForm
+@onready var error_label: Label = %ErrorLabel
 
 
 func _on_create_button_pressed() -> void:
 	buttons.hide()
+	error_label.hide()
 	loading_spinner.show()
 	var data = infra_config_form.get_data()
 	var response = await Http.send_request(
@@ -24,6 +26,8 @@ func _on_create_button_pressed() -> void:
 	if not response.is_successful():
 		buttons.show()
 		loading_spinner.hide()
+		error_label.text = response.get_error()
+		error_label.show()
 		return
 	dialog.completed.emit()
 	dialog.queue_free()
